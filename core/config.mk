a67755571c365b2aa403f68ebe7e66c90a74617d
Luca Stefani <luca020400@lineageos.org>
2017-08-17 22:20:29 +0200

    core: Include QCOM targets
    Change-Id: I7e33db775e80f042ef71a43c67f32abd4b6038e7
    Adapted for the QCOM macros that live in UA vendor
    Signed-off-by: D. Andrei Măceș <dmaces@nd.edu>

--- core/config.mk
+++ core/config.mk
@@ -958,4 +958,7 @@ include $(BUILD_SYSTEM)/ninja_config.mk
 include $(BUILD_SYSTEM)/soong_config.mk
 endif
 
+# Rules for QCOM targets
+-include $(TOPDIR)vendor/unlegacy/build/core/qcom_target.mk
+
 include $(BUILD_SYSTEM)/dumpvar.mk
